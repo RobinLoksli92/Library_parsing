0913da6fb574d71eecdc36e7df8dac631b8f91ba
RobinLoksli92 <gleb11tiun@mail.ru>
2021-10-12 18:33:28 +0200

    download books covers.

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
+from urllib import parse
+from requests.models import HTTPError
+import os
 from pathlib import Path
 import requests
+from urllib.parse import urljoin, urlsplit
+from urllib.parse import urlparse
+from pathvalidate import sanitize_filename
 
 from bs4 import BeautifulSoup
 
-from requests.models import HTTPError
-
-from bs4_tutorial import download_txt
 
 def check_for_redirect(response):
     if response.history:
         raise requests.HTTPError
 
 
+def download_txt(response, filename, folder ='books/'): 
+    sanitazed_filename = sanitize_filename(filename)
+    filepath = os.path.join(folder, sanitazed_filename)
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
+    return filepath
+
+
+def download_image(image_url, filename, folder='images/'):
+    response = requests.get(image_url)
+    response.raise_for_status
+    sanitazed_filename = sanitize_filename(filename)
+    filepath = os.path.join(folder, sanitazed_filename)
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
+    return filepath
+
+
 books_path = 'books/'
 Path(books_path).mkdir(parents=True, exist_ok=True)
-
+images_path = 'images/'
+Path(images_path).mkdir(parents=True, exist_ok=True)
 
 for book_id in range(1,11):
     books_url = f'https://tululu.org/b{book_id}'
@@ -24,20 +46,22 @@ for book_id in range(1,11):
     title_tag = soup.find('h1')
     title_text = title_tag.text
     splitted_title_text = title_text.split('::')
-    title = splitted_title_text[0].strip()
-
+    book_title = splitted_title_text[0].strip()
+    
     download_books_url = f'https://tululu.org/txt.php?id={book_id}'
     download_books_response = requests.get(download_books_url)
     download_books_response.raise_for_status
 
-    # try:
-    #     check_for_redirect(download_books_response)
-    #     filename = f'{book_id}. {title}.txt'
-    #     filepath = download_txt(download_books_url, filename)
-        
-    #     with open(filepath, 'wb') as file:
-    #         file.write(download_books_response.content)       
-    # except requests.HTTPError:
-    #     pass
+    try:
+        check_for_redirect(download_books_response)
+        filename = f'{book_id}. {book_title}.txt'
+        # download_txt(download_books_response, filename)
+        book_image = soup.find(class_='bookimage').find('img')['src']
+        image_url = urljoin('https://tululu.org/', book_image)
+        image_name = book_image.split('/')[-1]
+        download_image(image_url, filename=image_name)
+
+    except requests.HTTPError:
+        pass
     
     
