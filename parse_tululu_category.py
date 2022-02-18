81a08dc5d81320cb41730c5607e10a352eeba8c7
RobinLoksli92 <gleb11tiun@mail.ru>
2022-02-18 12:34:17 +0200

    Скрипт принимает страницы для скачивания

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os
 from pprint import pprint
@@ -15,38 +16,46 @@ from main import parse_book_page
 book_discription = []
 
 
-with open('book_discription.json','r', encoding='utf-8') as file:
-    for book in json.load(file):
-        book_discription.append(book)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--start_page',default=1, type=int)
+    parser.add_argument('--end_page', default=701, type=int)
+    args = parser.parse_args()
 
-for page in range(1,2):
-    url = f'http://tululu.org/l55/{page}'
-    response = requests.get(url)
-    response.raise_for_status()
-    fantastic_books = response.text
-    soup = BeautifulSoup(fantastic_books, 'lxml')
-    # books = soup.find_all(class_='d_book')
-    imagelink_selector = '.d_book a[href*="/b"]'
-    books_links = soup.select(imagelink_selector)
-    for book_link in books_links:
-        book_link = book_link['href']
-        book_url = f'http://tululu.org{book_link}'
-        try:
-            book_response = requests.get(book_url)
-            book_response.raise_for_status()
-            check_for_redirect(book_response)
-            soup = BeautifulSoup(book_response.text, 'lxml')
-            some_book = parse_book_page(soup)
-            book_discription.append(some_book)
-            book_title = some_book['Заголовок']
-            image_url = some_book['Картинка']
-            image_url_path = urlsplit(image_url).path
-            image_name = os.path.split(image_url_path)[-1]
-            book_id = ''.join([symbol for symbol in book_link if symbol.isdigit()])
-            download_txt(book_id, filename=book_title)
-            download_image(image_url, book_id, filename=image_name)
-        except requests.HTTPError:
-            pass
-with open('book_discription.json','w', encoding='utf-8') as file:
-    json.dump(book_discription, file, ensure_ascii=False)
+    with open('book_discription.json','r', encoding='utf-8') as file:
+        for book in json.load(file):
+            book_discription.append(book)
 
+    for page in range(args.start_page, args.end_page):
+        url = f'http://tululu.org/l55/{page}'
+        response = requests.get(url)
+        response.raise_for_status()
+        fantastic_books = response.text
+        soup = BeautifulSoup(fantastic_books, 'lxml')
+        imagelink_selector = '.d_book a[href*="/b"]'
+        books_links = soup.select(imagelink_selector)
+        for book_link in books_links:
+            book_link = book_link['href']
+            book_url = f'http://tululu.org{book_link}'
+            try:
+                book_response = requests.get(book_url)
+                book_response.raise_for_status()
+                check_for_redirect(book_response)
+                soup = BeautifulSoup(book_response.text, 'lxml')
+                some_book = parse_book_page(soup)
+                book_discription.append(some_book)
+                book_title = some_book['Заголовок']
+                image_url = some_book['Картинка']
+                image_url_path = urlsplit(image_url).path
+                image_name = os.path.split(image_url_path)[-1]
+                book_id = ''.join([symbol for symbol in book_link if symbol.isdigit()])
+                download_txt(book_id, filename=book_title)
+                download_image(image_url, book_id, filename=image_name)
+            except requests.HTTPError:
+                pass
+    with open('book_discription.json','w', encoding='utf-8') as file:
+        json.dump(book_discription, file, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    main()
